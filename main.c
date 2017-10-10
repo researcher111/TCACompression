bd1b173b54a115c3aa3183af9d340ee95bfb3f39
Daniel Graham <researcher111@users.noreply.github.com>
2017-10-10 12:55:14 -0400

    Update main.c
    Small optimization

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define EMPTY 0
+
+
 //28
-static char *Table = 0;
+static char *Table = EMPTY;
 static int tIndex = 0;
 
 char * compressPacket(char data[], int TableSize, char *Table) {
-    if (Table == 0) {
-        Table = malloc(TableSize);
+    if (Table == EMPTY) {
+        Table = calloc(TableSize);
     }
-    char *out = malloc(8);
+    char *out = calloc(8);
     static int place = 0;
     int i;
     for (i = 0; i < 8; i++) {
-        char index;
-        for (index = 0; index < TableSize && Table[index] != data[i]; index++) {}
+        //Searching the Compression table for the matching, key value pair. 
+        //Continues until it has found a matching pair. 
+        char index = 0;
+        while(index < TableSize && Table[index] != data[i]){
+            index++; 
+        }
+        //We have got the last index so the table does not contain a key, value pair for the packet.
         if (index == TableSize) {
             printf("update ");
             tIndex++;
@@ -22,22 +30,24 @@ char * compressPacket(char data[], int TableSize, char *Table) {
             Table[tIndex] = data[i];
             int j;
             for (j = 3; j >= 0; j--) {
-                char mask = 3;
+                char mask = 0x03; // Mask 00000011
                 mask = mask << (j * 2);
                 char partial = data[i] & mask;
                 partial = partial >> (j * 2);
+                
+                //TODO: update shifting. 
                 int shift = (31 - place) % 4;
                 char x = partial << shift * 2;
+                
                 char *byteout;
                 int offset = place / 4;
                 byteout = (out + offset);
-                if (shift == 3)
-                    *byteout = 0;
                 *byteout = *byteout | x;
                 place++;
             }
         } else {
             printf("compressed ");
+            //TODO: This about the math from calculating placement. 
             int shift = (31 - place) % 4;
             char x = index << shift * 2;
             char *byteout;
